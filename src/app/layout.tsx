fbe545d02c09084a50ed6d1f8d84983336519bc5
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-06-26 18:50:50 +0100

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import ScrollToTop from "../../components/hooks/scroll-to-top";
 import { animationCreate } from "../../components/utils/utils";
 import "react-modal-video/css/modal-video.css";
+import AppData from "../data/app.json";
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
@@ -23,7 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>conference-tim</title>
+        <title>{AppData.settings.siteName}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <link
           href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700|Rubik:400,500,700"
