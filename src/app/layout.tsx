f37ef004fefd061fe9b1cd4d41493926b5cb2cfb
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-06-26 17:41:45 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-"use client"
-import "../../styles/index.css"; 
+"use client";
+import "../../styles/index.css";
 import { useEffect } from "react";
 import ScrollToTop from "../../components/hooks/scroll-to-top";
 import { animationCreate } from "../../components/utils/utils";
-import 'react-modal-video/css/modal-video.css';
+import "react-modal-video/css/modal-video.css";
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
@@ -11,27 +11,29 @@ if (typeof window !== "undefined") {
 
 export default function RootLayout({
   children,
-}:{ children: React.ReactNode }) 
-{
+}: {
+  children: React.ReactNode;
+}) {
   useEffect(() => {
     setTimeout(() => {
       animationCreate();
     }, 500);
   }, []);
-  
+
   return (
     <html lang="en">
       <head>
         <title>TIM'24</title>
-        
+
         <link
           href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700|Rubik:400,500,700"
-          rel="stylesheet" />
-      </head> 
+          rel="stylesheet"
+        />
+      </head>
       <body>
         {children}
         <ScrollToTop />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
