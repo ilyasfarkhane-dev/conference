21fbd6c7b378d681176c42e4e9d552330efe4d79
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-06-28 12:16:12 +0100

    program

--- components/contact/GoogleMap.tsx
+++ components/contact/GoogleMap.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import ProgressBar from '../progress/progress';
+import React from "react";
+import ProgressBar from "../progress/progress";
 
 const GoogleMap = () => {
   const containerStyle: React.CSSProperties = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: '100vh',
-    backgroundColor: '#f8f9fa'
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "100vh",
+    backgroundColor: "#f8f9fa",
   };
 
   const innerContainerStyle: React.CSSProperties = {
-    width: '50%',
-    marginTop: '100px'
+    width: "50%",
+    marginTop: "0px",
   };
 
   const headingStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#28a745',
-    fontWeight: 'bold',
-    marginBottom: '1rem'
+    textAlign: "center",
+    color: "#28a745",
+    fontWeight: "bold",
+    marginBottom: "1rem",
   };
 
   return (
-    <section style={{ margin: '0', padding: '0' }}>
+    <section style={{ margin: "0", padding: "0" }}>
       <div style={containerStyle}>
         <div style={innerContainerStyle}>
-          <h1 style={headingStyle}>En cours</h1>
+          <h1 style={headingStyle}>Comming Soon</h1>
           <ProgressBar targetProgress={70} />
         </div>
       </div>
